24aece17c311bb86395998df25fb1b0160308998
radalis-Takumi <gold.ra.d.alis.12@gmail.com>
2023-08-28 03:59:54 +0900

    Calenderオブジェクトを追加

--- cv2withpp.py
+++ cv2withpp.py
@@ -1,5 +1,8 @@
 import cv2
 import numpy as np
+import calendar
+import jpholiday
+import datetime
 from PIL import Image, ImageDraw, ImageFont
 import math
 
@@ -146,6 +149,45 @@ class Textbox:
 
         return img
 
+class Triangle:
+    def __init__(self, cpt, width, height, rotate=0, fillcolor=None, framecolor=None):
+        self.cpt = cpt
+        self.width = width
+        self.height = height
+        self.rotate = rotate
+        self.fillcolor = fillcolor
+        self.framecolor = framecolor
+        self.para_change()
+    
+    def obj_move(self, cpt):
+        self.pts = []
+        for pt in self.b_pts:
+            self.pts.append([round(cpt[0] + pt[0]), round(cpt[1] + pt[1])])
+    
+    def obj_rotete(self, rotate):
+        self.b_pts = [[            0, -self.height * 2/3],
+                      [ self.width/2,  self.height/3], 
+                      [-self.width/2,  self.height/3]]
+        radians = -math.radians(rotate) # 回転方向を一般的な感覚（反時計回り）に変更
+        for i, pt in enumerate(self.b_pts):
+            self.b_pts[i] = [math.cos(radians)*pt[0] - math.sin(radians)*pt[1], math.sin(radians)*pt[0] + math.cos(radians)*pt[1]]
+    
+    def para_change(self, rotate=None, cpt=None):
+        if rotate:
+            self.rotate = rotate % 360
+        if cpt:
+            self.cpt = cpt
+        self.obj_rotete(self.rotate)
+        self.obj_move(self.cpt)
+
+    def draw(self, img):
+        pts = np.array(self.pts)
+        if self.fillcolor:
+            cv2.fillPoly(img, [pts], self.fillcolor, lineType=cv2.LINE_AA)
+        if self.framecolor:
+            cv2.polylines(img, [pts], True, self.framecolor, thickness=1, lineType=cv2.LINE_AA)
+        return img
+
 class Rectangle:
     def __init__(self, cpt, width, height, rad=0, rotate=0, fillcolor=None, framecolor=None):
         self.cpt = cpt
@@ -155,7 +197,7 @@ class Rectangle:
         self.rotate = rotate % 360
         self.fillcolor = fillcolor
         self.framecolor = framecolor
-        self.para_change(rad=self.rad)
+        self.para_change()
         
     def obj_move(self, cpt):
         self.pts1 = []
@@ -233,6 +275,57 @@ class Ellipse:
             cv2.ellipse(img, self.cpt, self.axes, -self.rotate, -self.startAngle, -self.endAngle, self.fillcolor, thickness=-1, lineType=cv2.LINE_AA)
         if self.framecolor:
             cv2.ellipse(img, self.cpt, self.axes, -self.rotate, -self.startAngle, -self.endAngle, self.framecolor, thickness=1, lineType=cv2.LINE_AA)
+
+class Calender:
+    def __init__(self, cpt, year, month, size=10):
+        self.year = year
+        self.month = month
+        self.cpt = cpt
+        self.size = size
+        self.ymUpdate()
+    
+    def ymUpdate(self, cpt=None, year=None, month=None):
+        if cpt:
+            self.cpt = cpt
+        if not year is None:
+            self.year = year
+        if not month is None:
+            self.month = month
+        self.txt_list = []
+        textm = f'{self.month}' if self.month > 9 else f' {self.month}'
+        self.txt_list.append(Textbox(f'{self.year}年  {textm}月', (self.cpt[0], self.cpt[1] - self.size * 7), 
+                                     'C:\Windows\Fonts\msgothic.ttc', self.size, (0,0,0), anchor='mm'))
+        for i, day in enumerate(['月', '火', '水', '木', '金', '土', '日']):
+            self.txt_list.append(Textbox(day, (self.cpt[0] - self.size * (6 - i * 2), self.cpt[1] - self.size * 5), 
+                                     'C:\Windows\Fonts\msgothic.ttc', self.size, (0,0,0), anchor='mm'))
+        for i, week_list in enumerate(calendar.monthcalendar(self.year, self.month)):
+            for j, day in enumerate(week_list):
+                if day == 0:
+                    continue
+                self.txt_list.append(Textbox(str(day), (self.cpt[0] - self.size * (6 - j * 2), self.cpt[1] - self.size * (3 - i * 2)), 
+                                     'C:\Windows\Fonts\msgothic.ttc', self.size, (0,0,0), anchor='mm'))
+
+    def draw(self, img):
+        cv2.rectangle(img, (round(self.cpt[0] - self.size * 7.3), round(self.cpt[1] - self.size * 8.3)), 
+                      (round(self.cpt[0] + self.size * 7.3), round(self.cpt[1] + self.size * 8.3)), (0, 0, 0), thickness=1)
+        cv2.rectangle(img, (round(self.cpt[0] - self.size * 7), round(self.cpt[1] - self.size * 8)), 
+                      (round(self.cpt[0] + self.size * 7), round(self.cpt[1] + self.size * 8)), (0, 0, 0), thickness=1)
+        cv2.rectangle(img, (self.cpt[0] - self.size * 4, self.cpt[1] - self.size * 8), (self.cpt[0] + self.size * 4, self.cpt[1] - self.size * 6),
+                       (0, 0, 0), thickness=1)
+        for i, week in enumerate([[0, 0, 0, 0, 0, 0, 0]] + calendar.monthcalendar(self.year, self.month)):
+            for j, day in enumerate(week):
+                if j == 5:
+                    cv2.rectangle(img, (self.cpt[0] - self.size * (7 - j * 2), self.cpt[1] - self.size * (6 - i * 2)),
+                       (self.cpt[0] - self.size * (5 - j * 2), self.cpt[1] - self.size * (4 - i * 2)), (255, 144, 30), thickness=-1)
+                elif j == 6 or (day > 0 and jpholiday.is_holiday(datetime.date(self.year, self.month, day))):  
+                    cv2.rectangle(img, (self.cpt[0] - self.size * (7 - j * 2), self.cpt[1] - self.size * (6 - i * 2)),
+                       (self.cpt[0] - self.size * (5 - j * 2), self.cpt[1] - self.size * (4 - i * 2)), (0, 0, 255), thickness=-1)
+                cv2.rectangle(img, (self.cpt[0] - self.size * (7 - j * 2), self.cpt[1] - self.size * (6 - i * 2)),
+                       (self.cpt[0] - self.size * (5 - j * 2), self.cpt[1] - self.size * (4 - i * 2)), (0, 0, 0), thickness=1)
+        for txtobj in self.txt_list:
+            txtobj.draw(img)
+        return img
+
 
 class Layer:
     def __init__(self, img):
